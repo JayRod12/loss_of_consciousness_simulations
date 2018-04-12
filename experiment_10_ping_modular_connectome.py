9716d82689095fb89c0a70045bcdeb63e873d542
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2018-04-12 16:32:47 +0200

    E10. Optimize synapse creation

--- experiment_10_ping_modular_connectome.py
+++ experiment_10_ping_modular_connectome.py
@@ -12,7 +12,6 @@ XYZ = spio.loadmat('data/coords_sporns_2mm.mat')['coords_new']
 N_MOD = len(XYZ)        # Number of modules
 
 # Parameters
-N_MOD = 200
 
 EX_EX_WEIGHT = 5*mV
 EX_IN_WEIGHT = 10*mV
@@ -63,7 +62,6 @@ echo = echo_start("Setting up synapses... \n")
 tt = time.time()
 EX_EX_SYN.connect(
     condition='int(i/40) == int(j/40)',
-    #condition='i >= ex_start and i < ex_end and j >= ex_start and j < ex_end',
     p=EX_CONNECTIVITY
 )
 print('\tEX_EX_SYN: {}s'.format(time.time() - tt))
@@ -71,51 +69,21 @@ print('\tEX_EX_SYN: {}s'.format(time.time() - tt))
 tt = time.time()
 EX_IN_SYN.connect(
     condition='j == int(i/4)'
-#    condition='i >= ex_start and i < ex_end and j == int(i/4)'
 )
 print('\tEX_IN_SYN: {}s'.format(time.time() - tt))
 
 tt = time.time()
 IN_EX_SYN.connect(
     condition='int(i/10) == int(j/40)'
-#    condition='i >= in_start and i < in_end and j >= ex_start and j < ex_end'
 )
 print('\tIN_EX_SYN: {}s'.format(time.time() - tt))
 
 tt = time.time()
 IN_IN_SYN.connect(
     condition='int(i/10) == int(j/10)',
-#    condition='i >= in_start and i < in_end and j >= in_start and j < in_end',
     p=IN_CONNECTIVITY
 )
 print('\tIN_IN_SYN: {}s'.format(time.time() - tt))
-
-#for module in range(N_MOD):
-#    ex_start, in_start = module * N_EX_MOD, module * N_IN_MOD
-#    ex_end, in_end = ex_start + N_EX_MOD, in_start + N_IN_MOD
-#    #a = time.time()
-#    #EX_EX_SYN.connect(
-#    #    condition='i >= ex_start and i < ex_end and j >= ex_start and j < ex_end',
-#    #    p=EX_CONNECTIVITY
-#    #)
-#    #print('EX_EX_SYN: {}s'.format(time.time() - a))
-#
-#    #a = time.time()
-#    #EX_IN_SYN.connect(
-#    #    condition='i >= ex_start and i < ex_end and j == int(i/4)'
-#    #)
-#    #print('EX_IN_SYN: {}s'.format(time.time() - a))
-#    #a = time.time()
-#    #IN_EX_SYN.connect(
-#    #    condition='i >= in_start and i < in_end and j >= ex_start and j < ex_end'
-#    #)
-#    #print('IN_EX_SYN: {}s'.format(time.time() - a))
-#    a = time.time()
-#    IN_IN_SYN.connect(
-#        condition='i >= in_start and i < in_end and j >= in_start and j < in_end',
-#        p=IN_CONNECTIVITY
-#    )
-#    print('IN_IN_SYN: {}s'.format(time.time() - a))
     
 EX_EX_SYN.w = EX_EX_WEIGHT
 EX_IN_SYN.w = EX_IN_WEIGHT
@@ -124,38 +92,44 @@ IN_IN_SYN.w = IN_IN_WEIGHT
 
 echo_end(echo)
 
-
 echo = echo_start("Running sym... ")
 M = SpikeMonitor(EX_G)
 duration = 5000*ms
 run(duration)
 echo_end(echo)
 
-echo = echo_start("Post-processing and plotting... ")
+echo = echo_start("Post-processing and plotting... \n")
 MEASURE_START = 1000
 MEASURE_DURATION = 500
 
+print("\t{} excitatory neuron spikes in total".format(len(M.t)))
+
+tt = time.time()
 X1, Y1 = [], []
 for spike_t, spike_idx in zip(M.t/ms, M.i):
     if MEASURE_START <= spike_t < MEASURE_START + MEASURE_DURATION and \
-            spike_idx < 40:
+            spike_idx < 3*40:
         X1.append(spike_t)
         Y1.append(spike_idx)
+print('\tCollect relevant spikes: {}s'.format(time.time() - tt))
 
-print("{} excitatory neuron spikes in total".format(len(X1)))
 #X, Y = M.t/ms, M.i
 X, Y = X1, Y1
 
+tt = time.time()
 dt = 10 # ms
 shift = 5 # ms
 total_steps = int(duration/(shift*ms))
 ma, time_scale = psd.moving_average(X, dt, shift, total_steps, True)
+print('\tCalculate moving average: {}s'.format(time.time() - tt))
 
+tt = time.time()
 X2, Y2 = [], []
 for ma_val, t in zip(ma, time_scale):
     if MEASURE_START <= t[0] < MEASURE_START + MEASURE_DURATION:
         Y2.append(ma_val)
         X2.append(t[0])
+print('\tCollect relvant moving average data points: {}s'.format(time.time() - tt))
 
 
 plt.subplot(211)
