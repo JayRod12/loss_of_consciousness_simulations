b820624bc1e08f58e34cb067e316a731841163ae
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2018-04-18 09:10:54 +0200

    E10. Add intermodular connections

--- experiment_10_ping_modular_connectome.py
+++ experiment_10_ping_modular_connectome.py
@@ -57,6 +57,10 @@ IN_IN_SYN = Synapses(IN_G, IN_G,
     on_pre='v += w',
     delay=DELAY
 )
+INTER_EX_EX_SYN = Synapses(EX_G, EX_G,
+    model='w: volt',
+    on_pre='v += w',
+)
 
 echo = echo_start("Setting up synapses... \n")
 
@@ -65,31 +69,62 @@ EX_EX_SYN.connect(
     condition='int(i/40) == int(j/40)',
     p=EX_CONNECTIVITY
 )
-print('\tEX_EX_SYN: {}s'.format(time.time() - tt))
+EX_EX_SYN.w = EX_EX_WEIGHT
+print('\tEX_EX_SYN ({:,} synapses): {}s'.format(len(EX_EX_SYN.w), time.time() - tt))
 
 tt = time.time()
 EX_IN_SYN.connect(
     condition='j == int(i/4)'
 )
-print('\tEX_IN_SYN: {}s'.format(time.time() - tt))
+EX_IN_SYN.w = EX_IN_WEIGHT
+print('\tEX_IN_SYN ({:,} synapses): {}s'.format(len(EX_IN_SYN.w), time.time() - tt))
 
 tt = time.time()
 IN_EX_SYN.connect(
     condition='int(i/10) == int(j/40)'
 )
-print('\tIN_EX_SYN: {}s'.format(time.time() - tt))
+IN_EX_SYN.w = IN_EX_WEIGHT
+print('\tIN_EX_SYN ({:,} synapses): {}s'.format(len(IN_EX_SYN.w), time.time() - tt))
 
 tt = time.time()
 IN_IN_SYN.connect(
     condition='int(i/10) == int(j/10)',
     p=IN_CONNECTIVITY
 )
-print('\tIN_IN_SYN: {}s'.format(time.time() - tt))
-    
-EX_EX_SYN.w = EX_EX_WEIGHT
-EX_IN_SYN.w = EX_IN_WEIGHT
-IN_EX_SYN.w = IN_EX_WEIGHT
 IN_IN_SYN.w = IN_IN_WEIGHT
+print('\tIN_IN_SYN ({:,} synapses): {}s'.format(len(IN_IN_SYN.w), time.time() - tt))
+
+# Synapses between modules (only excitatory-excitatory connections will be
+# created)
+INTER_MODULE_CONNECTIVITY = 0.1
+
+tt = time.time()
+
+synapses = []
+delay_matrix = np.zeros((N_MOD, N_MOD))
+for i in range(N_MOD):
+    x, y, z = XYZ[i]
+    for j in range(N_MOD):
+        if CIJ[i][j] > 0:
+            # Delay = distance / speed, speed = 2 m/s
+            x2, y2, z2 = XYZ[j]
+            delay_matrix[i][j] = math.sqrt((x-x2)**2 + (y-y2)**2 + (z-z2)**2)/2
+            base_i, base_j = i * N_EX_MOD, j * N_EX_MOD
+            synapses += [(base_i + ii, base_j + jj)
+                for ii in range(N_EX_MOD)
+                for jj in range(N_EX_MOD)
+                if sample() < INTER_MODULE_CONNECTIVITY
+            ]
+
+synapses_i, synapses_j = map(np.array, zip(*synapses))
+INTER_EX_EX_SYN.connect(i=synapses_i, j=synapses_j)
+
+INTER_EX_EX_SYN.delay = \
+        delay_matrix[np.array(synapses_i/N_EX_MOD), np.array(synapses_j/N_EX_MOD)] * ms
+
+INTER_EX_EX_SYN.w = CIJ[synapses_i/N_EX_MOD, synapses_j/N_EX_MOD] * mV
+
+print('\tINTER_EX_EX_SYN ({:,} synapses): {}s'.format(len(INTER_EX_EX_SYN.w), time.time() - tt))
 
 echo_end(echo)
 
@@ -100,12 +135,13 @@ run(duration)
 echo_end(echo)
 
 
-fname = "experiment_data/exp10_{}sec.pickle".format(duration/ms/1000)
+fname = "experiment_data/exp10_{}sec.pickle".format(int(duration/ms/1000))
 echo = echo_start("Storing data to {}... ".format(fname))
 DATA = {
     'X': np.array(M.t/ms),
     'Y': np.array(M.i),
-    'duration': duration/ms
+    'duration': duration/ms,
+    'N_MOD': N_MOD
 }
 
 with open(fname, 'wb') as f:
