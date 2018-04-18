9bc5c7b06ed48f87a5dbee5b7631e422d38b24a9
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2018-04-18 08:57:27 +0200

    Simplify logic in moving average method

--- power_spectral_density.py
+++ power_spectral_density.py
@@ -2,63 +2,62 @@ from scipy.signal import welch
 import numpy as np
 
 # y = f(t)
-def moving_average(t, dt, shift, total_steps, return_time=False):
-    if len(t) == 0:
-        return []
+# Returns the number of events per time unit averaged over a window of size `dt` and
+# at every time point separated by `shift` time units.
+# Parameters
+#   In:
+#       - a: [float] - Array of floats representing the times at which an event occurs
+#       - dt: int - Window of time over which events are counted
+#       - shift: int - Time shift between every measurement
+#       - start: int - Number of time units at which the measurement starts
+#       - end: int - Number of time units at which the measurement ends
+#   Out:
+#       - output: [float] - Moving average of the rate of events. Represents number of 
+#                           events per time unit calculated as an average over the window.
+#       - ts: [float] - Time steps representing the start time at which every output
+#                       measurement is recorded.
+def moving_average(a, dt, shift, start, end):
+    assert len(a) > 0
 
-    N = len(t)
-    MA = []
-    Time = []
-    t0 = 0
-    t1 = t0 + dt
-    l = 0
+    # time steps
+    ts = np.linspace(start, end, (end - start) / shift, endpoint=False)
+    output = np.zeros(len(ts), dtype=np.float)
 
-    while l < N:
-        # Find next interval [t0, t1)
+    N, M = len(a), len(ts)
+
+    i, j = 0, 0
+
+    while i < N and j < M:
+        # Find all spikes in interval [ts[j], ts[j] + dt)
+        t0, t1 = ts[j], ts[j] + dt
 
         # Find first index >= t0
-        if t[l] < t0:
-            while l + 1 < N and t[l + 1] < t0:
-                l += 1
-            l = l + 1
+        if a[i] < t0:
+            while i + 1 < N and a[i + 1] < t0:
+                i += 1
+            i += 1
 
-        if l == N:
+        if i == N:
             break
 
-        # If first index >= t1, then it's outside the slot
-        if t[l] >= t1:
-            ma = 0
+        # If first index >= t1, then it's outside the current slot
+        if a[i] >= t1:
+            output[j] = 0
         else:
             # Find the last index inside the time slot
-            r = l
-            while r + 1 < N and t[r + 1] < t1:
-                r += 1
-            ma = r-l+1
+            k = i
+            while k + 1 < N and a[k + 1] < t1:
+                k += 1
+            output[j] = k - i + 1
+        j += 1
         #print("[{}, {}) -> [{},{}]: {}".format(t0, t1, t[l], max(t[l], t[r]), ma))
 
-        MA.append(ma)
-        Time.append((t0, t1))
-        t0 = t0 + shift
-        t1 = t0 + dt
-
-    MA = np.array(MA, dtype=np.float)
-    if total_steps > len(MA):
-        # Pad array with zeros at the end until a length of 'total_steps'
-        MA = np.pad(MA, (0, total_steps-len(MA)), 'constant')
-        for step in range(total_steps-len(MA)):
-            Time.append((t0, t1))
-            t0 = t0 + shift
-            t1 = t0 + dt
-
     # Calculate # spikes / time step i.e. the average
-    MA /= dt
-    if return_time:
-        return (MA, Time)
-    else:
-        return MA
+    output /= dt
+    return output, ts
 
-def power_spectrum(t, dt, shift, total_steps):
-    ma = moving_average(t, dt, shift, total_steps)
+def power_spectrum(a, dt, shift, start, end):
+    ma, _ = moving_average(a, dt, shift, start, end)
     # e.g. Shift = 20ms
     # Therefore the sampling frequency is 1/20ms = 1 / 0.02s = 50Hz
     return welch(ma, int(1.0/(float(shift)/1000)))
