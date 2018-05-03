491b1f3ddecbcc7ce69565f1f82175e8dcb111fe
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2018-05-03 15:43:39 +0200

    Ex 9 Refactor script into a function

--- experiment_9_single_module.py
+++ experiment_9_single_module.py
@@ -1,4 +1,5 @@
 from brian2 import *
+from echo_time import *
 from neuron_groups import *
 from izhikevich_constants import *
 from numpy.random import random_sample
@@ -19,98 +20,133 @@ import power_spectral_density as psd
 #np.random.seed(177876383)
 #np.random.seed(177735)
 np.random.seed(25)
-DELAY = 5*ms
 
-N_EX = 40
-N_IN = 10
+def run_simulation(ex_current, in_current, ex_conn=0.4, in_conn=0.1):
+    DELAY = 5*ms
 
-EX_CONNECTIVITY = 0.4
-IN_CONNECTIVITY = 0.1
+    N_EX = 40
+    N_IN = 10
 
-EX_G = ExcitatoryNeuronGroup(N_EX)
-EX_G.I = 15*random_sample(N_EX)*mV/ms
+    EX_CONNECTIVITY = ex_conn
+    IN_CONNECTIVITY = in_conn
 
-IN_G = InhibitoryNeuronGroup(N_IN)
-IN_G.I = 3*random_sample(N_IN)*mV/ms
+    EX_G = ExcitatoryNeuronGroup(N_EX)
+    EX_G.I = ex_current*random_sample(N_EX)*mV/ms
 
-
-# PARAMS
-EX_EX_WEIGHT = 5*mV
-EX_IN_WEIGHT = 10*mV
-IN_EX_WEIGHT = -10*mV
-IN_IN_WEIGHT = -10*mV
-
-# 1-to-1 connections: EX_CONNECTIVITY% of all synapses
-EX_EX_SYN = Synapses(EX_G,
-    model='w: volt',
-    on_pre='v += w',
-    delay=DELAY
-)
-EX_EX_SYN.connect(p=EX_CONNECTIVITY)
-EX_EX_SYN.w = EX_EX_WEIGHT
+    IN_G = InhibitoryNeuronGroup(N_IN)
+    IN_G.I = in_current*random_sample(N_IN)*mV/ms
 
 
-# Many-to-one connection: 4 excitatory to 1 inhibitory
-EX_IN_SYN = Synapses(EX_G, IN_G,
-    model='w: volt',
-    on_pre='v += w',
-    delay=DELAY
-)
-EX_IN_SYN.connect(j='int(i/4)')
-EX_IN_SYN.w = EX_IN_WEIGHT
+    # PARAMS
+    EX_EX_WEIGHT = 5*mV
+    EX_IN_WEIGHT = 10*mV
+    IN_EX_WEIGHT = -10*mV
+    IN_IN_WEIGHT = -10*mV
+
+    echo = echo_start("Setting up synapses... \n")
+
+    # 1-to-1 connections: EX_CONNECTIVITY% of all synapses
+    EX_EX_SYN = Synapses(EX_G,
+        model='w: volt',
+        on_pre='v += w',
+        delay=DELAY
+    )
+    tt = time.time()
+    EX_EX_SYN.connect(p=EX_CONNECTIVITY)
+    EX_EX_SYN.w = EX_EX_WEIGHT
+    print('\tEX_EX_SYN ({:,} synapses): {}s'.format(len(EX_EX_SYN.w), time.time() - tt))
 
 
-# All-to-all: diffuse inhibitory to excitatory connections
-IN_EX_SYN = Synapses(IN_G, EX_G,
-    model='w: volt',
-    on_pre='v += w',
-    delay=DELAY
-)
-IN_EX_SYN.connect(p=1.0)
-IN_EX_SYN.w = IN_EX_WEIGHT
+    # Many-to-one connection: 4 excitatory to 1 inhibitory
+    EX_IN_SYN = Synapses(EX_G, IN_G,
+        model='w: volt',
+        on_pre='v += w',
+        delay=DELAY
+    )
+    tt = time.time()
+    EX_IN_SYN.connect(j='int(i/4)')
+    EX_IN_SYN.w = EX_IN_WEIGHT
+    print('\tEX_IN_SYN ({:,} synapses): {}s'.format(len(EX_IN_SYN.w), time.time() - tt))
 
 
-# 1-to-1: IN_CONNECTIVITY% of all synapses
-IN_IN_SYN = Synapses(IN_G, IN_G,
-    model='w: volt',
-    on_pre='v += w',
-    delay=DELAY
-)
-IN_IN_SYN.connect(p=IN_CONNECTIVITY)
-IN_IN_SYN.w = IN_IN_WEIGHT
+    # All-to-all: diffuse inhibitory to excitatory connections
+    IN_EX_SYN = Synapses(IN_G, EX_G,
+        model='w: volt',
+        on_pre='v += w',
+        delay=DELAY
+    )
+    tt = time.time()
+    IN_EX_SYN.connect(p=1.0)
+    IN_EX_SYN.w = IN_EX_WEIGHT
+    print('\tIN_EX_SYN ({:,} synapses): {}s'.format(len(IN_EX_SYN.w), time.time() - tt))
 
-# Monitoring and simulation
-M = SpikeMonitor(EX_G)
-duration = 5000*ms
-run(duration)
 
-t_start = 1000
-t_end = t_start + 500
+    # 1-to-1: IN_CONNECTIVITY% of all synapses
+    IN_IN_SYN = Synapses(IN_G, IN_G,
+        model='w: volt',
+        on_pre='v += w',
+        delay=DELAY
+    )
+    tt = time.time()
+    IN_IN_SYN.connect(p=IN_CONNECTIVITY)
+    IN_IN_SYN.w = IN_IN_WEIGHT
+    print('\tIN_IN_SYN ({:,} synapses): {}s'.format(len(IN_IN_SYN.w), time.time() - tt))
 
-X1, Y1 = [], []
-for spike_t, spike_idx in zip(M.t/ms, M.i):
-    if t_start <= spike_t < t_end:
-        X1.append(spike_t)
-        Y1.append(spike_idx)
+    echo_end(echo)
 
-X, Y = M.t/ms, M.i
+    # Monitoring and simulation
+    echo = echo_start("Running sym... ")
+    M_EX = SpikeMonitor(EX_G)
+    M_IN = SpikeMonitor(IN_G)
+    M_V = StateMonitor(EX_G, 'v', record=[0,1])
+    duration = 5000*ms
+    run(duration)
+    echo_end(echo)
 
-dt = 10 # ms
-shift = 5 # ms
-total_steps = int(duration/(shift*ms))
-ma, time_scale = psd.moving_average(X, dt, shift, t_start, t_end)
 
-X2, Y2 = time_scale, ma
+    start_time = 1000
+    end_time = 1500
 
-plt.subplot(211)
-plt.plot(X1, Y1, '.b')
-plt.ylabel('Neuron Index')
-plt.xlabel('Simulation Time (ms)')
+    echo = echo_start("Processing data and plotting... ")
+    X, Y = M_EX.t/ms, M_EX.i
+    mask = np.logical_and(X >= start_time, X < end_time)
+    X, Y = X[mask], Y[mask]
+    dt, shift = 10, 5 # ms
+    ma, time_scale = psd.moving_average(X, dt, shift, start_time, end_time)
 
-plt.subplot(212)
-plt.plot(X2, Y2)
-plt.xlabel('Simulation Time (ms)')
-plt.ylabel('Mean firing rate')
+    plt.subplot(221)
+    plt.plot(X, Y, '.b')
+    plt.xlabel('Simulation Time')
+    plt.ylabel('Neuron Index (Ex)')
 
-plt.show()
+    plt.subplot(222)
+    plt.plot(time_scale, ma)
+    plt.xlabel('Simulation Time (ms)')
+    plt.ylabel('Mean firing rate (Ex)')
+
+    X_IN, Y_IN = M_IN.t/ms, M_IN.i
+    mask = np.logical_and(X_IN >= start_time, X_IN < end_time)
+    X_IN, Y_IN = X_IN[mask], Y_IN[mask]
+    dt, shift = 10, 5 # ms
+    ex_ma, ex_time_scale = psd.moving_average(X_IN, dt, shift, start_time, end_time)
+
+    plt.subplot(223)
+    plt.plot(X_IN, Y_IN, '.b')
+    plt.xlabel('Simulation Time (ms) (In)')
+    plt.ylabel('Neuron Index (In)')
+
+    plt.subplot(224)
+    plt.plot(ex_time_scale, ex_ma)
+    plt.xlabel('Simulation Time (ms)')
+    plt.ylabel('Mean firing rate (In)')
+
+    plt.show()
+
+    echo_end(echo)
+    return (X, Y, X_IN, Y_IN, (M_V.t/ms, M_V.v[0]))
+
+if __name__ == '__main__':
+    ex_current = 15
+    in_current = 3
+    run_experiment(ex_current, in_current)
 
