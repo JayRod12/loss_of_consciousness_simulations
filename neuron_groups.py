c4f1f6c07d4363c721c63c71ab5fc4b1305be99f
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2018-05-17 15:05:56 +0200

    Merge izhikevich_constants and neuron_groups

--- neuron_groups.py
+++ neuron_groups.py
@@ -4,14 +4,17 @@ from izhikevich_constants import *
 # Neuron groups with heterogeneity as presented in Simple Model of Spiking
 # Neurons, IEEE Transactions on Neural Networks (2003) 14:1569-1572,
 # Eugene Izhikevich.
-
-def ExcitatoryNeuronGroup(N):
-    G = NeuronGroup(N,
-        EXCITATORY_NEURON_EQS,
-        threshold=THRES_EQ,
-        reset=EXCITATORY_RESET_EQ,
+def createGroup(N):
+    return NeuronGroup(N,
+        izhikevich_equations,
+        threshold=threshold_equations,
+        reset=reset_equations,
         method='rk4'
     )
+
+
+def ExcitatoryNeuronGroup(N):
+    G = createGroup()
     G.rnd = 'rand()'
     G.a = 0.02/ms
     G.b = 0.2/ms
@@ -20,12 +23,7 @@ def ExcitatoryNeuronGroup(N):
     return G
 
 def InhibitoryNeuronGroup(N):
-    G = NeuronGroup(N,
-        INHIBITORY_NEURON_EQS,
-        threshold=THRES_EQ,
-        reset=INHIBITORY_RESET_EQ,
-        method='rk4'
-    )
+    G = createGroup(N)
     G.rnd = 'rand()'
     G.a = '(0.02 + 0.08 * rnd) / ms'
     G.b = '(0.25 - 0.05 * rnd) / ms'
@@ -33,4 +31,22 @@ def InhibitoryNeuronGroup(N):
     G.d = 2 * mV / ms
     return G
 
+Vt=30*mV
 
+izhikevich_equations = '''
+  dv/dt=(0.04/(mV*ms))*(v**2)+(5/ms)*v+140*mV/ms-u+I : volt
+  du/dt=a*(b*v-u) : volt/second
+  I : volt/second
+  rnd : 1
+  a : 1/second
+  b : 1/second
+  c : volt
+  d : volt/second
+'''
+  
+reset_equations = '''
+  v=c
+  u=u+d
+'''
+
+threshold_equations = 'v>=Vt'
