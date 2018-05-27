e8110977562e5cf277883767564cacf1e68b9168
Jaime Rodriguez <jaime.rodriguez1295@gmail.com>
2018-05-27 11:04:32 +0200

    Optional echo message

--- echo_time.py
+++ echo_time.py
@@ -2,7 +2,7 @@
 import sys
 import time
 
-ECHO_OFF = True
+ECHO_OFF = False
 def echo_start(msg):
     t = time.time()
     if not ECHO_OFF:
@@ -10,8 +10,8 @@ def echo_start(msg):
         sys.stdout.flush()
     return t
 
-def echo_end(t):
+def echo_end(t, opt_text=""):
     elapsed = time.time() - t
     if not ECHO_OFF:
-        sys.stdout.write(" [{:.2f}s]\n".format(elapsed))
+        sys.stdout.write("{} [{:.2f}s]\n".format(opt_text, elapsed))
         sys.stdout.flush()
